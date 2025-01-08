fd9c09953298aa738c21f6fb11bc3f51f4e326a2
SokWith <129967025+SokWith@users.noreply.github.com>
2025-01-08 21:21:02 +0800

    Update core.js

--- cf01/web240/web/js/bing/chat/core.js
+++ cf01/web240/web/js/bing/chat/core.js
@@ -17,7 +17,7 @@ _w.rms.js(
   { 'A:rms:answers:CodexBundle:cib-bundle': '/rp/cyU_OCO3RbZ2qeKuF-N208x6HCE.br.js' },
   { 'A:rms:answers:SharedStaticAssets:speech-sdk': '/rp/5RUBu5dEYIDzWQtuZRVAHjBjuyc.br.js' },
   { 'A:rms:answers:Web:SydneyWelcomeScreenBase':'/rp/J1Qt_5kw_tmsXwaKeAFz2xPNi4s.br.js' },
-  { 'A:rms:answers:Web:SydneyWelcomeScreen':'/rp/sNzL8vfHHNmyqYOSQey9fNYd3kI.br.js' },
+ // { 'A:rms:answers:Web:SydneyWelcomeScreen':'/rp/sNzL8vfHHNmyqYOSQey9fNYd3kI.br.js' },
   { 'A:rms:answers:Web:SydneyFullScreenConv': '/rp/0UCEcxL4L_MIx61KSqZ5vt5H4V0.br.js' },
   { 'A:rms:answers:Web:ChatHomeScreenBase': '/rp/-rTLoTob7E0ltIUEW9nZQHmVMYk.br.js' },
   { 'A:rms:answers:Web:ChatHomeScreen': '/rp/iKLEB3hIDCT4236vCJrv0iHxANU.br.js' },
